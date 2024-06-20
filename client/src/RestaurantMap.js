918f92bd982e3794e7b0ee8a7339ca3de89df3f3
Saatvik Rao <saatvik.rao@iitgn.ac.in>
2024-06-20 15:46:28 +0530

    added map view adjuster based on search

--- client/src/RestaurantMap.js
+++ client/src/RestaurantMap.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
+const MapViewAdjuster = ({ locations }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (locations.length > 0) {
+      const bounds = locations.map(loc => [loc.location.coordinates[1], loc.location.coordinates[0]]);
+      map.fitBounds(bounds, { padding: [50, 50] });  // add padding to zoom out slightly
+    }
+  }, [locations, map]);
+
+  return null;
+};
 
 const RestaurantMap = () => {
   const [latitude, setLatitude] = useState('');
@@ -51,6 +64,7 @@ const RestaurantMap = () => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
+        {restaurants.length > 0 && <MapViewAdjuster locations={restaurants} />}
         {restaurants.map((restaurant) => (
           <Marker key={restaurant._id} position={[restaurant.location.coordinates[1], restaurant.location.coordinates[0]]}>
             <Popup>
